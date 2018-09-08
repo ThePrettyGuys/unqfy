2eedab37da091fac934044abfa30e9592e74ff26
Marcos <marcos.alvarenga@10pines.com>
2018-09-08 00:13:28 -0300

    Comentarios sobre cosas a implementar a un archivo de texto enriquecido

--- main.js
+++ main.js
@@ -20,45 +20,10 @@ function saveUNQfy(unqfy, filename = 'data.json') {
     unqfy.save(filename);
 }
 
-/*
- En esta funcion deberán interpretar los argumentos pasado por linea de comandos
- e implementar los diferentes comandos.
-
-  Se deberán implementar los comandos:
-    -/ Alta y baja de Artista HECHO
-    - Alta y Baja de Albums  HECHO ALTA. **************Falta baja*****************
-    - Alta y Baja de tracks HECHO ALTA   **************Falta baja*****************
-
-    -/ Listar todos los Artistas  HECHO.
-    - Listar todos los albumes de un artista  
-    - Listar todos los tracks de un album
-
-    -/ Busqueda de canciones intepretadas por un determinado artista HECHO.
-    - Busqueda de canciones por genero
-
-    - Dado un string, imprimmir todas las entidades (artistas, albums, tracks, playlists) que coincidan parcialmente
-    con el string pasado.
-
-    - Dada un nombre de playlist, una lista de generos y una duración máxima, crear una playlist que contenga
-    tracks que tengan canciones con esos generos y que tenga como duración máxima la pasada por parámetro.
-
-  La implementacion de los comandos deberá ser de la forma:
-   1. Obtener argumentos de linea de comando
-   2. Obtener instancia de UNQfy (getUNQFy)
-   3. Ejecutar el comando correspondiente en Unqfy
-   4. Guardar el estado de UNQfy (saveUNQfy)
-
-*/
-
-function deleteUnnusedKeys() {
-    Object.assign(parsedArgs, { $0: undefined, _: undefined });
-    return parsedArgs;
+function deleteUnnusedKeys(arguments) {
+    Object.assign(arguments, { $0: undefined, _: undefined });
+    return arguments;
 }
-
-/**
- * En esta función se deben registrar los handlers necesarios para cada comando disponible.
- *
- */
 
 /**El comando se debe ingresar de la forma:
  *  > node main.js command parametro1="valor" parametro2="valor"
@@ -76,7 +41,7 @@ function main() {
     let command = parsedArgs._[0];
 
     if(command){
-        let objectByParameters = deleteUnnusedKeys();
+        let objectByParameters = deleteUnnusedKeys(parsedArgs);
         let commandHandler = commandSelector.findHandler(command);
 
         if(commandHandler){
