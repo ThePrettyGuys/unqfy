1187d4c2fba135eeabaa0c5e06336d13aafa16e6
Marcos <marcos.alvarenga@10pines.com>
2018-09-08 05:58:52 -0300

    Test para validar la elección del handler

---
+++ test/commandSelectorTest.js
@@ -0,0 +1,32 @@
+/* eslint-env node, mocha */
+
+const assert = require('chai').assert;
+const CommandSelector = require('../command/commandSelector');
+const HandlersCreator = require('../command/handlersCreator');
+
+describe('CommandSelector create with all handlers', () => {
+    let handlersToRegister = HandlersCreator.create();
+    let commandSelector = new CommandSelector(handlersToRegister);
+
+    describe('#findHandler', () => {
+        describe('with valid command received', ()=>{
+            let commandsCandidatesToReceive = ['AddArtist', 'AddAlbum', 'AddTrack', 'DeleteArtist', 'SearchSongsFrom', 'ShowAllArtists', 'ShowAllArtistAlbum'];
+
+            commandsCandidatesToReceive.forEach(commandReceived => {
+                it(`should be ${commandReceived}`, () => {
+                    const handler = commandSelector.findHandler(commandReceived);
+                    assert.equal(commandReceived, handler.command);
+                });
+            })
+        });
+
+        describe('with invalid command received', ()=>{
+            let commandReceived = 'Invalid';
+
+            it('should be undefined', () => {
+                const handler = commandSelector.findHandler(commandReceived);
+                assert.equal(handler, undefined);
+            });
+        });
+    })
+});
