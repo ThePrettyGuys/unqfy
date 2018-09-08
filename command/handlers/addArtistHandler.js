06ac733447dd67e1e1eb7462a64e3e1c1850b390
Marcos <marcos.alvarenga@10pines.com>
2018-09-08 03:00:22 -0300

    Se validan los datos del artista antes de agregar, y no se permite hacerlo si no tiene la forma correcta. No chequea si son valores válidos.

--- command/handlers/addArtistHandler.js
+++ command/handlers/addArtistHandler.js
@@ -8,10 +8,22 @@ class AddArtistHandler {
     }
 
     handle(unqfy, artistData) {
-        console.log('Se agregará al artista con name: {' + artistData.name + '} y country: {' + artistData.country + '}.');
-        unqfy.addArtist(artistData);
-        console.log('Artista agregado satisfactoriamente.');
-        return unqfy;
+        if(this.validateArtistData(artistData)){
+            console.log('Se agregará al artista con name: {' + artistData.name + '} y country: {' + artistData.country + '}.');
+            unqfy.addArtist(artistData);
+            console.log('Artista agregado satisfactoriamente.');
+            return unqfy;
+        }
+    }
+
+    validateArtistData(artistData) {
+        let hasData = Boolean(artistData);
+        let hasCompleteData = Boolean((artistData || {}).name) && Boolean((artistData || {}).country);
+
+        if (!hasData || !hasCompleteData) {
+            console.log('No se pudo completar la operación. Los datos son incorrectos: ' + (artistData || {}).name + ' ' + (artistData || {}).country);
+        }
+        return hasData && hasCompleteData;
     }
 }
 
