198826ec865250f0b24c00b974c73c7caff12d3d
Marcos <marcos.alvarenga@10pines.com>
2018-09-09 18:53:18 -0300

    Se agrega validacion de los datos recibidos para crear la playlist

--- command/handlers/createPlaylistHandler.js
+++ command/handlers/createPlaylistHandler.js
@@ -9,14 +9,23 @@ class CreatePlaylistHandler {
     }
 
     handle(unqfy, playlistData) {
-        //Esto lo voy a refactorear mucho mas lindo con mas info linda al usuario
-        if(playlistData.name === undefined || playlistData.genres === undefined || playlistData.maxDuration === undefined){
-            console.log("Faltan parametros!")
-        }else{
-            console.log(unqfy.createPlaylist(playlistData.name, playlistData.genres, playlistData.maxDuration));
+        if(this.validateData(playlistData)){
+            console.log(`Se creará la playlist: { ${name} }`);
+            let {name, genres, maxDuration} = playlistData;
+            unqfy.createPlaylist(name, genres, maxDuration);
+            console.log(`Se creó la playlist: { ${name} } de duracion maxima: { ${maxDuration} }, para los generos: { ${genres} }`);
         }
     }
 
+    validateData(data) {
+        let hasData = Boolean(data);
+        let hasCompleteData = Boolean((data || {}).name) && Boolean((data || {}).genres) && Boolean((data || {}).maxDuration);
+
+        if (!hasData || !hasCompleteData) {
+            console.log(`No se pudo completar la operación. Los datos son incorrectos: ${(data || {}).name }, ${(data|| {}).genres}, ${(data|| {}).maxDuration}`);
+        }
+        return hasData && hasCompleteData;
+    }
 }
 
 module.exports = CreatePlaylistHandler;
