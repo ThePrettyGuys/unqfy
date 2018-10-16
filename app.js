45552d6682590077be69902b3e2155ebdc312cac
RedondaAnalia <a.redonda89@gmail.com>
2018-10-16 17:14:02 -0300

    Server funcionando con peticion a localhost:3000/

---
+++ app.js
@@ -0,0 +1,47 @@
+/**
+ * HowTo: Definir nuevas rutas:
+ * - Definir la ruta en la carpeta rutas, segun buenas practicas.
+ * - Importarla en este archivo, en la parte de Importar rutas
+ * - Declararla en la app, mediante app.use(), debajo de la importacion de rutas.
+ */
+
+// Requires
+var express= require ('express');
+var bodyParser= require ('body-parser');
+
+
+// Inicializar variables
+var app = express();
+
+// CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+    next();
+});
+
+
+// Body Parser
+//parse application/ x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false}));
+//parse application/json
+app.use(bodyParser.json());
+
+
+// Importar rutas
+var appRoutes = require('./routes/app');
+
+
+// Rutas
+/**
+ * Es posicional.... La raiz siempre debe quedar a lo ultimo!
+ */
+app.use('/', appRoutes);
+
+
+
+// Escuchar peticiones
+app.listen(3000, () => {
+    console.log('Express Server puerto 3000: \x1b[32m%s\x1b[0m', 'online');
+});
