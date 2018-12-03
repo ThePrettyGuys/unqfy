dd69c64878dd34f294c086f229c2342ca2e0ee33
Unknown <marcos.alvarenga@10pines.com>
2018-12-03 11:35:28 -0300

    Fix de enpoint de loggin

--- unqfy/services/endpoints.js
+++ unqfy/services/endpoints.js
@@ -2,4 +2,4 @@ exports.SPOTIFYURL = 'https://api.spotify.com/v1/';
 exports.MUSIXMATCHURL= 'http://api.musixmatch.com/ws/1.1/';
 exports.MUSIXMATCHURL2= 'https://developer.musixmatch.com/documentation/api-reference/track-search';
 exports.NOTIFY_ENDPOINT = 'http://172.20.0.22:5001/api/notify';
-module.exports.LOGGINGURL= 'http://localhost:7000/api/log';
+module.exports.LOGGINGURL= 'http://172.20.0.23:5002/api/log';
