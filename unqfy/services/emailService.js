992b902673fe9a4f41dee88f082acef2732d0106
Unknown <marcos.alvarenga@10pines.com>
2018-12-03 15:05:22 -0300

    Se agrega la notificación por mail al eliminar un album

--- unqfy/services/emailService.js
+++ unqfy/services/emailService.js
@@ -2,25 +2,39 @@ const notifyEndpoint = require('../services/endpoints').NOTIFY_ENDPOINT;
 const email_from = require('../../config/config').email_from;
 const rp = require('request-promise');
 
-class EmailService{
-    notifyNewAlbum(album,artist){
+class EmailService {
+    notifyNewAlbum(album, artist) {
+        const subject = `Nuevo album de ${artist.name}`;
+        const message = `Se agregó el album ${album.name} de ${artist.name}`;
+
+        return this.notificar(artist, album, subject, message);
+    }
+
+    notifyDeleteAlbum(album, artist) {
+        const subject = `Albúm eliminado de ${artist.name}`;
+        const message = `Se elimino el album ${album.name}, del artista: ${artist.name}`;
+
+        return this.notificar(artist, album, subject, message);
+    }
+
+    notificar(artist, album, subject, message, onfulfilled = () => {
+        console.log('Mail Enviado');
+    }, onrejected = () => {
+        console.log('No se envió el mail');
+    }) {
         const options = {
             url: notifyEndpoint,
             body: {
                 artistId: `${artist.id}`,
                 from: email_from,
-                subject: `Nuevo album de ${artist.name}`,
-                message: `Se agregó el album ${album.name} de ${artist.name}`
+                subject: subject,
+                message: message
             },
             json: true
         };
         return rp.post(options)
-        .then( () => { console.log("Mande el email")} )
-        .catch(() => { console.log("No mande el email")})
-    }
-
-    notifyDeleteAlbum(album,artist){
-        return
+            .then(onfulfilled)
+            .catch(onrejected);
     }
 }
 
